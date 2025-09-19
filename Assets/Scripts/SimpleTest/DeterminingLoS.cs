d2631389ffeaf8a94f6d97b2234ba71546f95934
Caliodore <caliodorecharbonneau@gmail.com>
2025-09-19 01:46:32 -0500

    Understanding Dot Product Usage
    Finally figured out how to properly use dot product to determine relative position.

--- Assets/Scripts/SimpleTest/DeterminingLoS.cs
+++ Assets/Scripts/SimpleTest/DeterminingLoS.cs
@@ -38,6 +38,8 @@ public class DeterminingLoS : MonoBehaviour
     public bool inverseBool;
     public bool drawDotGuardForward;
 
+    public LayerMask layerMaskTest;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -46,6 +48,8 @@ public class DeterminingLoS : MonoBehaviour
         relativeXMagnitude = Mathf.Cos(60);
         rightSideOfViewArc = new Vector3(relativeXMagnitude, 0, guardForwardVector.z);
         leftSideOfViewArc = new Vector3(-relativeXMagnitude, 0, guardForwardVector.z);
+
+        layerMaskTest = LayerMask.GetMask("Default", "RaycastTest");
     }
 
     // Update is called once per frame
@@ -57,6 +61,7 @@ public class DeterminingLoS : MonoBehaviour
         inverseDistanceToPlayer = directionToPlayer.x;
 
         dotProduct = Vector3.Dot(transform.forward, directionToPlayer);
+        Vector3 splinkVector = new Vector3(dotProduct, 0, dotProduct).normalized;
         oppositeDotProduct = Vector3.Dot(directionToPlayer, transform.forward);
         oppositeDotVector = new Vector3(-oppositeDotProduct, 0, transform.forward.z);
         dotForwardVector = new Vector3(dotProduct, 0, transform.forward.z);
@@ -64,7 +69,8 @@ public class DeterminingLoS : MonoBehaviour
         angleBetweenForwardPlayer = Vector3.Angle(transform.forward, directionToPlayer);
         distanceToPlayer = Vector3.Distance(guardPosition, playerPosition);
 
-
+        Vector3 dotDoubleVec = new Vector3(dotProduct, 0, 1).normalized;
+        Vector3 dotDoubleVecNeg = new Vector3(1, 0, dotProduct).normalized;
 
         Vector3 addForward = transform.forward * distanceToPlayer;
         addForward = transform.position + addForward;
@@ -76,16 +82,25 @@ public class DeterminingLoS : MonoBehaviour
         //Debug.DrawRay(guardPosition, rightSideOfViewArc, Color.red);
         //Debug.DrawRay(guardPosition, transform.forward * distanceToPlayer, Color.green); 
         //Debug.DrawRay(guardPosition, leftSideOfViewArc, Color.red);
-        Vector3 halfPosVector = new Vector3(0.5f, 0, 1);
-        Vector3 halfNegVector = new Vector3(-0.5f, 0, 1);
+        Vector3 halfPosVector = new Vector3(0.66f, 0, 1);
+        Vector3 halfNegVector = new Vector3(-0.66f, 0, 1);
 
         Debug.DrawRay(guardPosition, halfPosVector, Color.black);
         Debug.DrawRay(guardPosition, halfNegVector, Color.black);
 
         Debug.DrawRay(guardPosition, oppositeDotVector, Color.red);
-        Debug.DrawRay(guardPosition, dotForwardVector, Color.yellow);
+        Debug.DrawRay(guardPosition, dotForwardVector, Color.red);
 
+        Debug.DrawRay(guardPosition, dotDoubleVec, Color.yellow);
+        //Debug.DrawRay(guardPosition, dotDoubleVecNeg, Color.cyan);
+        Debug.DrawRay(guardPosition, -dotDoubleVec, Color.yellow);
+        //Debug.DrawRay(guardPosition, -dotDoubleVecNeg, Color.cyan);
 
+        Debug.DrawRay(guardPosition, directionToPlayer.normalized, Color.cyan);
+
+        Debug.DrawRay(guardPosition, splinkVector, Color.magenta);
+
+        //So when dotProduct <= viewAngle.x is what we are tracking.
     }
 
     private void FixedUpdate()
@@ -94,15 +109,19 @@ public class DeterminingLoS : MonoBehaviour
         { 
             //Vector3 normalizedPlayerDirVector = directionToPlayer.normalized;
             //float angleBetween = Vector3.Angle(normalizedPlayerDirVector, transform.forward);
-
+            if(dotProduct >= 0.66f)
+            { 
+                raycastResult = Physics.Raycast(guardPosition, directionToPlayer, Mathf.Infinity, layerMaskTest);
+                Debug.DrawLine(guardPosition, playerPosition, Color.green);
+            }
         }
         if(drawLineBool)
         { 
             //Debug.DrawLine(guardPosition, playerPosition, Color.red);
             if(inverseBool)
             { 
-                Debug.DrawRay(guardPosition, inversePlayerToGuardTransform, Color.cyan);
-                Debug.DrawRay(playerPosition, inverseGuardToPlayerTransform, Color.red);
+                //Debug.DrawRay(guardPosition, inversePlayerToGuardTransform, Color.cyan);
+                //Debug.DrawRay(playerPosition, inverseGuardToPlayerTransform, Color.red);
             }
             if(drawDotGuardForward)
             {
